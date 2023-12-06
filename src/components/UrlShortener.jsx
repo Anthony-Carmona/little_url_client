29317087b4c24f4ff4d50bfe3ee8bcda7db85a74
HollyMaguire <hollygracemaguire@gmail.com>
2023-12-06 12:09:34 -0800

    style update

--- src/components/UrlShortener.jsx
+++ src/components/UrlShortener.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
+import { Panel } from 'rsuite';
 
 export default function UrlShortener() {
 
@@ -23,23 +24,25 @@ export default function UrlShortener() {
     }
 
     return (
-    <div>
-    <div class="grid gap-6 mb-6 md:grid-cols-2">
-    <div class="mb-6">
-        <label for="url" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">litify your url</label>
-        <input type="text" id="url" value={url} onChange={handleChange} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
-        <button onClick={ handleSubmit } class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-    </div>
+    <Panel className="panel">
+    <div className="header">
+        <label for="url">litify your url</label>
+        </div> 
+        <div>
+        <input className="inputBox" type="text" id="url" value={url} placeholder="Input a url to shorten"onChange={handleChange} required/>
+
+    </div> 
+     <button onClick={ handleSubmit } className="submit">Submit</button>
+    <div>Your little url:</div>
     { little_url &&
-      <div>
-        your little_url: {little_url}
+      <div className="littleUrl">
+     {little_url}
       </div>
     }
-    </div>
 
     {url}
+    </Panel>
 
-    
-</div>
     )
+    
   }
